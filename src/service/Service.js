4e04404ec5a1b3b3aeccf7094c631fc8e0f5bccb
tushig <tushig.btr@gmail.com>
2022-02-16 11:33:28 -0600

    dax pair fixed

--- src/service/Service.js
+++ src/service/Service.js
@@ -6,8 +6,13 @@ export const API = 'http://139.59.225.83:3000/api/';
 axios.interceptors.request.use(
   async config => {
     config.headers['Content-Type'] = 'application/json';
-    const token = await AsyncStorage.getItem('accessToken');
-    if (token) config.headers = {Authorization: 'Bearer ' + token};
+    if (
+      config.url.includes('139.59.225.83') ||
+      config.url.includes('coinbell')
+    ) {
+      const token = await AsyncStorage.getItem('accessToken');
+      if (token) config.headers = {Authorization: 'Bearer ' + token};
+    }
     return config;
   },
   error => {
